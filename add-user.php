bda8b8fa80f74245029da0569b9cbc013756a9d6
Mohibul880 <mdmohibulislam880@gmail.com>
2025-12-05 16:18:19 +0600

    add_users_update

--- add-user.php
+++ add-user.php
@@ -3,28 +3,26 @@ require_once "functions/functions.php";
 get_header();
 get_sidebar();
 
+$message = ""; // message holder
+
 if(!empty($_POST)){
   
   $Name       = $_POST['Name'];
   $Phone      = $_POST['Phone'];
   $Email      = $_POST['Email'];
   $User_Name  = $_POST['User_Name'];
-  $Password   = $_POST['Password'];  // ফর্ম থেকে আসবে, কিন্তু ডাটাবেজে যাবে না
 
-  // ডাটাবেজে PASSWORD বাদ দেওয়া হলো
   $insert = "INSERT INTO users (Name, Phone, Email, User_Name)
              VALUES ('$Name', '$Phone', '$Email', '$User_Name')";
-
+  
   $q = mysqli_query($conn, $insert);
- 
+
   if($q){
-      echo "User Registration Successful";
+      $message = "<div class='alert alert-success'>User Registration Successful</div>";
   }else{
-      echo "User Registration Failed: " . mysqli_error($conn);
+      $message = "<div class='alert alert-danger'>User Registration Failed: " . mysqli_error($conn) . "</div>";
   }
-
 }
-
 ?>
 
 <div class="col-md-10 content">
@@ -32,12 +30,16 @@ if(!empty($_POST)){
         <div class="col-md-12 breadcumb_part">
             <div class="bread">
                 <ul>
-                    <li><a href=""><i class="fas fa-home"></i>Home</a></li>
-                    <li><a href=""><i class="fas fa-angle-double-right"></i>Dashboard</a></li>                             
+                  <li><a href=""><i class="fas fa-home"></i>Home</a></li>
+                  <li><a href=""><i class="fas fa-angle-double-right"></i>Dashboard</a></li>                             
                 </ul>
             </div>
         </div>
     </div>
+
+    <!-- SHOW MESSAGE HERE -->
+    <?php echo $message; ?>
+
     <div class="row">
         <div class="col-md-12 ">
             <form method="post" action="">
@@ -53,69 +55,45 @@ if(!empty($_POST)){
                     </div>
                   </div>
                   <div class="card-body">
+                      
                       <div class="row mb-3">
                         <label class="col-sm-3 col-form-label col_form_label">Name<span class="req_star">*</span>:</label>
                         <div class="col-sm-7">
-                          <input type="text" class="form-control form_control" id="" name="Name">
+                          <input type="text" class="form-control form_control" name="Name">
                         </div>
                       </div>
+
                       <div class="row mb-3">
                         <label class="col-sm-3 col-form-label col_form_label">Phone:</label>
                         <div class="col-sm-7">
-                          <input type="text" class="form-control form_control" id="" name="Phone">
+                          <input type="text" class="form-control form_control" name="Phone">
                         </div>
                       </div>
+
                       <div class="row mb-3">
                         <label class="col-sm-3 col-form-label col_form_label">Email<span class="req_star">*</span>:</label>
                         <div class="col-sm-7">
-                          <input type="email" class="form-control form_control" id="" name="Email">
+                          <input type="email" class="form-control form_control" name="Email">
                         </div>
                       </div>
+
                       <div class="row mb-3">
                         <label class="col-sm-3 col-form-label col_form_label">Username<span class="req_star">*</span>:</label>
                         <div class="col-sm-7">
-                          <input type="text" class="form-control form_control" id="" name="User_Name">
+                          <input type="text" class="form-control form_control" name="User_Name">
                         </div>
                       </div>
-                      <div class="row mb-3">
-                        <label class="col-sm-3 col-form-label col_form_label">Password<span class="req_star">*</span>:</label>
-                        <div class="col-sm-7">
-                          <input type="password" class="form-control form_control" id="" name="">
-                        </div>
-                      </div>
-                      <div class="row mb-3">
-                        <label class="col-sm-3 col-form-label col_form_label">Confirm-Password<span class="req_star">*</span>:</label>
-                        <div class="col-sm-7">
-                          <input type="password" class="form-control form_control" id="" name="">
-                        </div>
-                      </div>
-                      <div class="row mb-3">
-                        <label class="col-sm-3 col-form-label col_form_label">User Role<span class="req_star">*</span>:</label>
-                        <div class="col-sm-4">
-                          <select class="form-control form_control" id="" name="">
-                            <option>Select Role</option>
-                            <option value="">Superadmin</option>
-                            <option value="">Admin</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div class="row mb-3">
-                        <label class="col-sm-3 col-form-label col_form_label">Photo:</label>
-                        <div class="col-sm-4">
-                          <input type="file" class="form-control form_control" id="" name="">
-                        </div>
-                      </div>
+
                   </div>
+
                   <div class="card-footer text-center">
                     <button type="submit" class="btn btn-sm btn-dark">REGISTRATION</button>
-                  </div>  
+                  </div>
+
                 </div>
             </form>
         </div>
     </div>
 </div>
-<?php
-get_footer();
 
-?>
-
+<?php get_footer(); ?>
